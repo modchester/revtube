e4b0226644e966f1748eecaa0967fa60b1268e25
forzasoft <96445516+forzasoft@users.noreply.github.com>
2022-01-05 22:42:29 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+    .logowh {
+        background-color: black;
+    }
+</style>
+<!-- Begin Jekyll SEO tag v2.6.1 -->
+<title>vistatube</title>
+<meta name="generator" content="Jekyll v3.9.0" />
+<meta property="og:title" content="vistatube" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:url" content="http://vt.redst0ne.xyz/" />
+<meta property="og:site_name" content="vistatube" />
+<!-- End Jekyll SEO tag -->
+
+    <link rel="stylesheet" href="https://lake-sketch.github.io/assets/css/style.css?v=105cc1030840380fa2690d4d168610561c6d93e4">
+  </head>
+  <body>
+    <div class="container-lg px-3 my-5 markdown-body">
+
+<img src="janbetablack.png" class="logowh">
+
+      <h1 id="lake-sketchgithubio">Welcome</h1>
+<p>Site is very unfinished, oh boy</p>
+
+
+      
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/anchor-js/4.1.0/anchor.min.js" integrity="sha256-lZaRhKri35AyJSypXXs4o6OPFTbTmUoltBbDCbdzegg=" crossorigin="anonymous"></script>
+    <script>anchors.add();</script>
+    
+  </body>
+</html>
