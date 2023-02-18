f9a41dc6e6926d6a7194c58cffd381e04e6ac54d
Cattskit <124375533+Cattskit@users.noreply.github.com>
2023-02-18 20:40:22 +0100

    d

--- report.php
+++ report.php
@@ -1,17 +1,15 @@
-<?php include './assets/mod/db.php';?>
 <?php include("./assets/mod/webhook.php"); ?>
-<h1>Report Video</h1>
-<form action="report.php" method="POST">
-        <input type="text" name="reason" placeholder="Reason">
-        <br>
-        <input type="text" name="sitelink" placeholder="Offending Video">
-        <br>
-        <input type="text" name="username" placeholder="Name of Channel with Video">
-        <br>
-        <h3><input type="submit" name="submit" value="Submit Report"></h3>
-        </form>
-        <br>
-        <?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php include './assets/mod/meta.php';?>
+</head>
+
+  <body>
+<?php include './assets/mod/db.php';?>
+    <?php include("./assets/mod/header.php");?>
+    <div class="container-flex"> 
+    <?php
 $proname = @$_POST["reason"];
 $link = @$_POST["sitelink"];
 $user = @$_POST["username"];
@@ -19,11 +17,11 @@ $user = @$_POST["username"];
 if(isset($_POST["submit"])){
 
     $dw = new DiscordWebhook($webhook);
+    $username = $_SESSION['profileuser3'];
 
     $dw->newMessage()
-    ->setContent("New report submitted")
+    ->setContent("**".$username."** just submitted a report:")
     ->setTitle("New Report")
-    ->setDescription($shit)
     ->setColor("#3238a8")
     ->addFields(
         ["Reason:", $proname, true],
@@ -31,6 +29,43 @@ if(isset($_POST["submit"])){
         ["User:", $user, true]
     )
     ->setTimestamp()
-    ->send();    
+    ->setUsername($username)
+    ->send(); 
 }
 ?>
+        <div class="col-1-2">
+            <br>
+            <br>
+            <br>
+            <br>
+            <center><h3>Report a video</h3>
+            <br>
+            <div class="card blue">
+                <form method="post" action="">
+                    <div class="input-group">
+                    <input type="text" name="reason" placeholder="Reason" required>
+                    </div>
+                    <br>
+                    <div class="input-group">
+                        <input type="text" name="sitelink" placeholder="Offending Video" required>
+                    </div>
+                    <br>
+                    <div class="input-group">
+                    <input type="text" name="username" placeholder="Name of Channel with Video" required>
+                    </div>
+                    <br>                   
+                    <div class="input-group">
+                    <input class="yt-button" type="submit" name="submit" value="Submit Report">
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!--<div class="col-1-2">
+            <h2>Welcome to SkyTube!</h2>
+        </div>-->
+    </div>
+    <hr>
+    <?php include("./assets/mod/footer.php") ?>
+</body>
+</html>
+<?php $mysqli->close();?>
