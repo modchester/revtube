4a05f51e285cf726cf28dfc94b62ba1865e89a45
Cattskit <124375533+Cattskit@users.noreply.github.com>
2023-02-19 12:10:30 +0100

    looks like someone forgot..... i'm looking at you risk.

--- videos.php
+++ videos.php
@@ -27,14 +27,14 @@
                         echo '
                             <div class="video container-flex">
                                 <div class="col-1-3 video-thumbnail">
-                                <a href="watch.php?id='.$row['id'].'">
+                                <a href="/watch?v='.$row['vid'].'">
                                     <video>
                                         <source src="../videos/'.$row['filename'].'" type="video/mp4">
                                         Thumbnail could not be loaded :(
                                     </video> 
                                 </a>
                                 </div>
-                                <div class="col-1-3 video-title"><a href="watch.php?id='.$row['id'].'">'.$row['videotitle'].'</a></div>
+                                <div class="col-1-3 video-title"><a href="/watch?id='.$row['vid'].'">'.$row['videotitle'].'</a></div>
                                 <div class="col-1-3 video-info">
                                     <div><a href="profile.php?id='.$row['author'].'">'.$row['author'].'</a></div>
                                     <div><span>'.$row['views'].'</span> views</div>
