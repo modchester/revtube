845da8d5aaaaaefca8545d2b17ef8531be79ab00
ky-1 <127993851+ky-1@users.noreply.github.com>
2023-06-17 15:56:55 -0400

    add classes to watch buttons for theming via stylus etc

--- assets/mod/footer.php
+++ assets/mod/footer.php
@@ -21,5 +21,6 @@ $total_time = round(($finish - $start), 4);
             ?>
     <p><abbr title="semi-2013 made by Cattskit and redst0ne, name by nomax">&copy; clipIt 2012-2023</abbr> <?php if ($debug == "true") { echo '&bull; <span style="color:red;">[DEBUG]</span> Running PHP '.$phpver.' | Users: '.$usercount.' | Videos: '.$videocount.' | Comments:'.$commentcount.' | Page loaded in '.$total_time.' seconds';}?></p>
     <!-- almost ready for launch i guess -redst0ne 05/04/23 -->
+    <!-- will people ever be willing to actually use this? -redst0ne 06/17/23 -->
 </footer>
 
