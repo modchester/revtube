c518a3be1ed75d9f599b6ab266ea7600bb33c587
ky-1 <127993851+ky-1@users.noreply.github.com>
2023-11-22 15:42:36 -0500

    admin crap

--- assets/lib/admin.php
+++ assets/lib/admin.php
@@ -6,23 +6,23 @@ function banUser($username, $reason, $mysqli) {
     // $stmt->execute();
     // $stmt->close();
     
-    $stmt = $mysqli->prepare("UPDATE users SET banned = '1' WHERE username = ?");
+    $stmt = $mysqli->prepare("UPDATE users SET strikes = '3' WHERE username = ?");
     $stmt->bind_param("s", $username);
     $stmt->execute();
     $stmt->close();
 
-    $stmt = $mysqli->prepare("UPDATE users SET banreason = ? WHERE username = ?");
-    $stmt->bind_param("ss", $reason, $username);
-    $stmt->execute();
-    $stmt->close();
+    // $stmt = $mysqli->prepare("UPDATE users SET banreason = ? WHERE username = ?");
+    // $stmt->bind_param("ss", $reason, $username);
+    // $stmt->execute();
+    // $stmt->close();
     
-    $stmt = $mysqli->prepare("UPDATE users SET bantime = now() WHERE username = ?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $stmt->close();
+    // $stmt = $mysqli->prepare("UPDATE users SET bantime = now() WHERE username = ?");
+    // $stmt->bind_param("s", $username);
+    // $stmt->execute();
+    // $stmt->close();
 }
 function unbanUser($username, $mysqli) {
-    $stmt = $mysqli->prepare("UPDATE users SET banned = '0' WHERE username = ?");
+    $stmt = $mysqli->prepare("UPDATE users SET strikes = '2' WHERE username = ?");
     $stmt->bind_param("s", $username);
     $stmt->execute();
     $stmt->close();
