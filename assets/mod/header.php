0b6bdb555090dee65b6cb6505ef262faa55828ad
ky-1 <127993851+ky-1@users.noreply.github.com>
2023-11-22 15:11:53 -0500

    studio (shouldve been added in jan 2022...smh)

--- assets/mod/header.php
+++ assets/mod/header.php
@@ -40,7 +40,7 @@ $start = $time;
               <li></li>
                 <li><a href=\"./profile?user=".$row["username"]."\">Your Channel</a></li>
                 <li><a href=\"upload\">Upload</a></li>
-                <li><a href='/inbox/index'>Inbox</a></li>
+                <li><a href='my_videos'>Studio</a></li>
                 <li class=\"divider\"></li>
                 <li><a href=\"account\">Settings</a></li>
                 ".$adminlink."
---
+++ my_videos.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php include './assets/mod/meta.php';?>      
+</head>
+
+  <body>
+<?php include './assets/mod/db.php';?>
+<?php include './assets/mod/header.php';?>
+    <div class="container">
+ <div class="content">
+        <div class="page-header">
+        <?php include './assets/mod/alert.php';?>
+          <h1>Studio <small><div id="clockbox"></div></small></h1>
+          <?php include './assets/mod/todaysdate.php'; ?>
+        </div>
+        <div class="row">
+          <div class="span10">
+                            <?php
+                    $statement = $mysqli->prepare("SELECT * FROM videos WHERE author = ? ORDER BY date DESC");
+                $statement->bind_param("s", $_SESSION['profileuser3']); 
+                $statement->execute();
+                $result = $statement->get_result();
+                if($result->num_rows !== 0){
+                  include("assets/lib/profile.php");
+                    while($row = $result->fetch_assoc()) {
+                      $likec = getLikes($row['vid'], $mysqli);
+    $dislikec = getDislikes($row['vid'], $mysqli);
+    $views = getViews($row['vid'], $mysqli); 
+    if($row['duration'] > 3600) {
+      $lengthlist = floor($row['duration'] / 3600) . ":" . gmdate("i:s", $row['duration'] % 3600);
+    } else { 
+      $lengthlist = gmdate("i:s", $row['duration'] % 3600) ;
+    };
+                        echo '
+                            <div class="video container-flex">
+                            <div class="editoptions">
+                            <a href="editvideo?v='.$row['vid'].'" id="editprof" style="margin-left: 44px; margin-top: 8px;" class="yt-button primary" type="button"><i class="bi bi-pencil-fill"></i> Edit</a> <a href="deletevideo?v='.$row['vid'].'" id="editprof" style="margin-left:0px;margin-top: 8px;" class="yt-button delete danger" type="button"><i class="bi bi-trash3-fill"></i> Delete</a>
+                            </div>
+                                <div class="col-1-3 video-thumbnail">
+                                <a href="/watch?v='.$row['vid'].'">
+                                <img height="70px" width="120px" src="content/thumb/' . $row['thumb'] . '">
+                                </a>
+                                </div>
+                                <div class="col-1-3 video-title"><a href="/watch?v='.$row['vid'].'"><b>'.$row['videotitle'].'</b></a></div>
+                                <div class="col-1-3 video-info">
+                                    <div><a href="profile?user='.$row['author'].'">'.$row['author'].'</a></div>
+                                    <div>'.$lengthlist.' &bull; '.$row['views'].' views &bull; <i class="bi bi-hand-thumbs-up-fill"></i> '.$likec.' <i class="bi bi-hand-thumbs-down-fill"></i> '.$dislikec.'</div>
+                                    <div><em>'.$row['description'].'</em></div>
+                                </div>
+                            </div>
+                            <hr>';
+                    }
+                }
+                else{
+                    echo "Hm. Interesting. Either nobody has uploaded yet or we have a serious problem o_o";
+                }
+                $statement->close();
+            ?>
+            <ul class="unstyled">
+
+            </ul>
+          </div>
+          <div class="span4">
+        <?php include './assets/mod/whatsnew.php'; ?>
+      </div>
+
+      
+
+    </div></div></div> <!-- /container -->
+    <?php include './assets/mod/footer.php'; ?>
+  </body>
+</html>
