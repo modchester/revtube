919acd9516e735ddd384fcf893851191025a3453
ky-1 <127993851+ky-1@users.noreply.github.com>
2023-11-22 15:47:37 -0500

    Update inboxwhatsnew.php

--- assets/mod/inboxwhatsnew.php
+++ assets/mod/inboxwhatsnew.php
@@ -1,3 +1,3 @@
 <a href="/inbox/compose" class="yt-button big"><i class="bi bi-pencil-square"></i> Compose</a>
 <h3>What's New</h3>
-            Welcome to sendIt, where you can message other clipIt users. This is currently in very early development.
+            Welcome to the inbox, where you can message other <?php echo $sitename; ?> users. This is currently in very early development.
