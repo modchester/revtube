c472962282d2173bc83cd1c93fd03c48f45f28d8
Kai <154871227+skyiebox@users.noreply.github.com>
2024-01-15 12:10:08 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -132,7 +132,7 @@
                     $verified = '';
                   }
                   echo('
-            <h3><h2>'.$row["username"].' '.$staff.' '.$verified.'</h2></h3>
+            <h3><h2>'.htmlspecialchars($row["username"]).' '.$staff.' '.$verified.'</h2></h3>
             <img id="prfp" style="height:225px;width:225px;" src="/content/pfp/' .getUserPic($pfp). '">
             '); 
       if(isset($_SESSION['profileuser3'])) {
