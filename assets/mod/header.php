2e02ef598df516a62cfca10cbc5165d7a0fe4ac2
ky-1 <127993851+ky-1@users.noreply.github.com>
2024-01-18 09:10:02 -0500

    better upload dropdown

--- assets/mod/header.php
+++ assets/mod/header.php
@@ -26,7 +26,7 @@ $start = $time;
           <div class="yt-button-group upload-group">
 	          <a class="pull-left uploadbtn yt-button" href="upload">Upload</a>
             <li class="dropdown" data-dropdown="dropdown">
-              <a href="#" class="dropdown-toggle pull-left uploadbtn yt-button"><i class="bi bi-caret-down-fill"></i></a>
+              <a href="#" class="dropdown-toggle pull-left uploadbtn uploaddrp yt-button"><i class="bi bi-caret-down-fill"></i></a>
               <ul class="dropdown-menu">
                 <li><a href="/my_videos">My Videos</a></li>
                 <li><a href="/profile?user=<?php echo htmlspecialchars($_SESSION['profileuser3']); ?>">My Channel</a></li>
