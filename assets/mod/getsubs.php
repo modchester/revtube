0a13c34f34e86f3cec74c8cde2eee096942184a1
Cattskit <131389951+Cattskit@users.noreply.github.com>
2024-02-16 02:28:29 +0100

    w

--- assets/mod/getsubs.php
+++ assets/mod/getsubs.php
@@ -5,12 +5,18 @@
                                           $stmt->bind_param("s", $_SESSION['profileuser3']);
                                           $stmt->execute();
                                           $result = $stmt->get_result();
-                                          if($result->num_rows !== 0){ echo "<span>Subscriptions ($result->num_rows)</span>"; }
-                                          
+                                          if($result->num_rows !== 0) { 
+                                            echo "<span>Subscriptions ($result->num_rows)</span>"; 
+                                            $hrPlease = true;
+                                          } 
                                           while($row = $result->fetch_assoc()) {
                                             $pid = idFromUser($row['receiver']);
                                             $rows = getSubscribers($row['receiver'], $mysqli);
 echo '<a href="/profile?user='.htmlspecialchars($row['receiver']).'"><li class="guide-item"><img style="vertical-align: bottom;" src="/content/pfp/' .getUserPic($pid). '" width="21px" height="19px"> '.htmlspecialchars($row['receiver']).'</li></a>';
                                           }
+                                        }
+
+                                        if(isset($hrPlease)) {
+                                          echo "<hr>";
                                         }
                                         ?>
