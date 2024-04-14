05f005023a6d9fecf3df9c147c6e53489eedbef1
Kylar <127993851+ky-1@users.noreply.github.com>
2024-04-14 16:24:10 -0400

    idiot....

--- profile.php
+++ profile.php
@@ -18,16 +18,17 @@
                 }
                 $username = htmlspecialchars($row['username']);
                 $description = htmlspecialchars($row['description']);
+                $id = idFromUser($username);
                 echo '<meta name="title" content="'.$username.' - '.$site['name'].'">
                 <meta name="description" content="'.$description.'">
                 <meta property="og:site_name" content="'.$site['name'].'" />
                 <meta property="og:title" content="'.$username.'">
                 <meta property="og:description" content="'.$description.'">
-                <meta property="og:image" content="/content/pfp/'.getUserPicN($username).'">
-                <meta property="og:url" content="profile.php?name='.$username.'">
+                <meta property="og:image" content="/content/pfp/'.getUserPic($id).'">
+                <meta property="og:url" content="profile.php?user='.$username.'">
                 <meta property="twitter:title" content="'.$username.'">
                 <meta property="twitter:description" content="'.$description.'">
-                <meta property="twitter:image" content="/content/pfp/'.getUserPicN($username).'">';
+                <meta property="twitter:image" content="/content/pfp/'.getUserPic($id).'">';
             }
             $statement->close();
         ?>
